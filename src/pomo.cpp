c5c7227f48c84b3d49166dd7291a728004641a05
Luna <3792747+SnowLune@users.noreply.github.com>
2026-07-20 17:01:08 -0500

    add .clang-tidy, replace font, begin C++ification to satisfy cppcoreguidelines

--- src/pomo.cpp
+++ src/pomo.cpp
@@ -1,131 +1,122 @@
 #include <iostream>
+#include <array>
 #include <chrono>
 #include <thread>
+#include <string>
 
 #include <raylib.h>
 
-const int FPS_TARGET		= 60;
+const int fps_target = 60;
 
-const float WORK_MINUTES 	= 25;
-const float SHORT_BREAK_MINUTES	= 5;
-const float LONG_BREAK_MINUTES	= 30;
+constexpr int work_interval_s =		25 * 60;
+constexpr int short_break_interval_s =	5 * 60;
+constexpr int long_break_interval_s =	30 * 60;
 
 // Durations for Work, Short Break, Long Break
-const int interval_s[3] = {
-	(int)(WORK_MINUTES * 60),
-	(int)(SHORT_BREAK_MINUTES * 60),
-	(int)(LONG_BREAK_MINUTES * 60)
+const std::array<int, 3> interval_s = { 
+	work_interval_s,
+	short_break_interval_s,
+	long_break_interval_s
 };
 
-const char* phase_names[3] = {
+const std::array<const char*, 3> phase_names = {
 	"WORK",
 	"SHORT BREAK",
 	"LONG BREAK"
 };
 
-enum phase
+enum class Phase
 {
-	work, short_break, long_break
+	work = 0, short_break = 1, long_break = 2
 };
 
-struct timer
+struct Timer
 {
 	Sound	tick_sound;
 	Sound	ring_sound;
-	float	current_time_s;
-	int	total_time_s;
-	int	work_count;
-	phase	phase;
-	bool	running;
+	float		current_time_s;
+	int		total_time_s;
+	int		work_count;
+	const char* 	phase_name;
+	Phase		phase;
+	bool		tick_muted;
+	bool		running;
 };
 
-timer
-create_timer (void)
+void
+set_timer (Timer* t, Phase phase)
 {
-	// Create
-	timer t;
+	const int phase_int = static_cast<int>(phase);
+	
+	t->phase = phase;
+	t->phase_name = phase_names.at(phase_int);
+	t->total_time_s = interval_s.at(phase_int);
+	t->current_time_s = 0.0;
+}
 
+Timer
+create_timer (const char* tick_sndfile, const char* ring_sndfile)
+{
+	Timer t {};
+	
 	// Initialize
-	t.phase = work;
-	t.total_time_s = interval_s[t.phase];
-	t.current_time_s = 0.0;
+	set_timer(&t, Phase::work);
 	t.running = false;
 	t.work_count = 0;
+	
+	// Load Sounds
+	if (IsAudioDeviceReady())
+	{
+		t.tick_sound = LoadSound(tick_sndfile);
+		t.ring_sound = LoadSound(ring_sndfile);
+	}
+	t.tick_muted = false;
+
+	const float tick_vol = 0.1;
+	SetSoundVolume(t.tick_sound, tick_vol);
 
 	return t;
 }
 
 void
-start_timer (timer* t)
-{
-	t->running = true;
-
-	while (t->current_time_s < t->total_time_s)
-	{
-		const short sec_ms = 1000;
-		std::this_thread::sleep_for(std::chrono::milliseconds(sec_ms));
-		
-		++(t->current_time_s);
-
-		std::cout << t->current_time_s
-			  << "/"
-			  << t->total_time_s
-			  << std::endl;
-	}
-
-	if (t->current_time_s >= t->total_time_s)
-	{
-		t->running = false;
-	}
-}
-
-void
-update_timer (timer* t, float ds)
+update_timer (Timer* t, float ds)
 {
 	t->current_time_s += ds;
 	
-	if (t->current_time_s >= t->total_time_s)
+	if ((int)t->current_time_s >= t->total_time_s)
 	{
-		if (t->phase == work)
+		if (t->phase == Phase::work)
 		{
 			if (t->work_count && t->work_count % 4 == 0)
-			{
-				t->phase = long_break;
-			}
+				set_timer(t, Phase::long_break);
 			else
-			{
-				t->phase = short_break;
-			}
+				set_timer(t, Phase::short_break);
 			
 			++t->work_count;
 		}
 		else
-			t->phase = work;
-
-		t->total_time_s = interval_s[t->phase];
-		t->current_time_s = 0.0;
+			set_timer(t, Phase::work);
 
 		// Play ring sound on interval completion
 		if (!IsSoundPlaying(t->ring_sound)) PlaySound(t->ring_sound);
 	}
 	else
 	{
-		if (!IsSoundPlaying(t->tick_sound)) PlaySound(t->tick_sound);
+		if (!t->tick_muted && !IsSoundPlaying(t->tick_sound))
+			PlaySound(t->tick_sound);
 	}
 }
 
 void
-stop_timer (timer* t)
+stop_timer (Timer* t)
 {
 	t->running = false;
 }
 
 void
-set_timer (timer* t, phase phase, float time)
+mute_timer_tick (Timer* t)
 {
-	t->phase = phase;
-	t->total_time_s = interval_s[t->phase];
-	t->current_time_s = time;
+	t->tick_muted = !t->tick_muted;
 }
 
 int
@@ -134,46 +125,45 @@ main (void)
 	const int screen_width = 640;
 	const int screen_height = 480;
 
-	const char* SOUND_TICK_FILE = "./sounds/timertick.ogg";
-	const char* SOUND_RING_FILE = "./sounds/timerring.ogg";
+	const float default_font_size = 32;
+	const float default_font_spacing = 0;
 
-	const char* FONT_FILE = "./fonts/Mx437_IBM_VGA_9x14.ttf";
+	const char* sound_tick_file = "./sounds/timertick.ogg";
+	const char* sound_ring_file = "./sounds/timerring.ogg";
 
-	timer pomodoro = create_timer();
-	timer* t = &pomodoro;
-	t->running = true;
-
-	InitWindow(screen_width, screen_height, "pomo");
-	SetTargetFPS(FPS_TARGET);
-
-	Font mono_font = LoadFont(FONT_FILE);
+	const char* font_file = "./fonts/JetBrainsMonoNerd.ttf";
 
 	InitAudioDevice();
-	t->tick_sound = LoadSound(SOUND_TICK_FILE);
-	t->ring_sound = LoadSound(SOUND_RING_FILE);
+	InitWindow(screen_width, screen_height, "pomo");
+	SetTargetFPS(fps_target);
+
+	Font mono_font = LoadFont(font_file);
+	
+	Timer pomodoro = create_timer(sound_tick_file, sound_ring_file);
+	Timer* t = &pomodoro;
+	t->running = true;
 
 	while (!WindowShouldClose())
 	{
-		const char* status_text = TextFormat(
-			"%s - %.3f/%d",
-			phase_names[t->phase],
-			t->current_time_s,
-			t->total_time_s
-		);
+		std::string status_text
+			= std::format(	"{} - {:8.2f}/{}",
+					t->phase_name,
+					t->current_time_s,
+					t->total_time_s	);
 
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
-		Vector2 text_pos = {10, 10};
-		DrawTextEx(
-			mono_font,
-			status_text,
-			text_pos,
-			14.25,
-			0.0,
-			BLACK
-		);
+		const Vector2 text_pos = {10, 10};
+		float font_size = default_font_size;
+		float font_spacing = default_font_spacing;
+		DrawTextEx(	mono_font,
+				status_text.c_str(),
+				text_pos,
+				font_size,
+				font_spacing,
+				BLACK	);
 
 		EndDrawing();
 
